e22a9b0aeca2996b77d7fd1715df40b812e55a1c
charlie on ubuntu 22.04 <charlie@elvenware.com>
2023-02-07 10:23:54 -0800

    getting started

---
+++ snippets/extensions.js
@@ -0,0 +1,41 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+const path = require('path');
+import { getWebpackConfig, createFile, formatCode, checkExists, updateDevDependencies } from './utils';
+
+
+export function activate(context) {
+    console.log('Congratulations, your extension "webpack" is now active!');
+    const disposable = vscode.commands.registerCommand('extension.createConfig', () => {
+        const rootPath = vscode.workspace.rootPath;
+
+        // if a folder is not open, then bail.
+        if (!rootPath) {
+            vscode.window.showErrorMessage('Please open a folder before generating a webpack file');
+        }
+
+        const webPackPath = path.join(rootPath, 'webpack.config.js')
+        if (checkExists(webPackPath)) {
+            return vscode.window.showInformationMessage('Webpack config already exists.');
+        }
+        const webPackConfig = formatCode(getWebpackConfig()); // get the webpack config
+        const isDevDepsUpdated = updateDevDependencies(); // update dev deps
+        if (createFile(webPackPath, webPackConfig) && isDevDepsUpdated) { // if written and updated
+            vscode.window.showInformationMessage('Webpack config created and dependencies update. Running npm install');
+            const terminal = vscode.window.createTerminal();
+            terminal.show();
+            return terminal.sendText('npm install');
+        }
+        return vscode.window.showErrorMessage('Something went wrong, please try again.');
+
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+    console.log('Webpack ext deactived.');
+}
---
+++ snippets/utils.js
@@ -0,0 +1,152 @@
+const fs = require('fs-extra');
+const path = require('path');
+import { js_beautify } from 'js-prettify';
+import { workspace } from 'vscode';
+const rootPath = workspace.rootPath;
+const packageFile = path.join(rootPath, 'package.json');
+
+const prettyConfig = {
+    indent_size: 2,
+    indent_char: " ",
+    indent_level: 0,
+    indent_with_tabs: false,
+    preserve_newlines: false,
+    max_preserve_newlines: 10,
+    jslint_happy: false,
+    brace_style: "collapse",
+    keep_array_indentation: false,
+    keep_function_indentation: false,
+    space_before_conditional: true,
+    break_chained_methods: false,
+    eval_code: false,
+    unescape_strings: false,
+    wrap_line_length: 0
+};
+
+export function checkExists(path) {
+    return fs.existsSync(path);
+}
+
+export function formatCode(content, config = prettyConfig) {
+    try {
+        return js_beautify(content, prettyConfig);
+    } catch (error) {
+        return console.log(error); // lets stop it here :(
+    }
+}
+
+export function copyFile(src, dest) {
+    try {
+        fs.copySync(path.resolve(src), dest);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+export function createFile(filePath, content, __JSON__ = false) {
+    try {
+        if (__JSON__) {
+            fs.outputJSONSync(filePath, content);
+            return true;
+        }
+        fs.outputFileSync(filePath, content);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+export function getAppPath() {
+    const appPaths = ['app', 'src'];
+    const appPath = appPaths.find(f => checkExists(path.join(rootPath, f)));
+    return appPath || 'app';
+}
+
+export function getBundlePath() {
+    const bundlePaths = ['dist', 'out', 'bundle'];
+    const bundlePath = bundlePaths.find(f => checkExists(path.join(rootPath, f)));
+    return bundlePath || 'dist';
+}
+
+export function getWebpackConfig() {
+    const appPath = getAppPath();
+    const bundlePath = getBundlePath();
+    return `
+  const path = require('path');
+  module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, '${appPath}', 'index'),
+    watch: true,
+    output: {
+      path: path.join(__dirname ,'${bundlePath}'),
+      publicPath: '/${bundlePath}/',
+      filename: "bundle.js",
+      chunkFilename: '[name].js'
+    },
+    module: {
+      rules: [{
+        test: /.jsx?$/,
+        include: [
+          path.resolve(__dirname, '${appPath}')
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              "@babel/env", {
+                "targets": {
+                  "browsers": "last 2 chrome versions"
+                }
+              }
+            ]
+          ]
+        }
+      }]
+    },
+    resolve: {
+      extensions: ['.json', '.js', '.jsx']
+    },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, '/${bundlePath}/'),
+      inline: true,
+      host: 'localhost',
+      port: 8080,
+    }
+  };
+  `;
+}
+
+export function updateDevDependencies() {
+    // if we dont have a package file, then no need to update.
+    if (!checkExists(packageFile)) {
+        return;
+    }
+
+    const newPackageInfo = Object.assign({}, require(packageFile));
+
+    const devDependencies = {
+        "@babel/core": "^7.18.13",
+        "@babel/preset-env": "^7.18.10",
+        "babel-loader": "^8.2.5",
+        "webpack": "^5.74.0",
+        "webpack-cli": "^4.10.0",
+        "webpack-dev-server": "^4.10.0"
+    };
+
+    newPackageInfo.devDependencies = Object.assign({}, newPackageInfo.devDependencies, devDependencies);
+
+    // write JSON to package.
+    try {
+        fs.writeJsonSync(packageFile, newPackageInfo, {
+            spaces: 2
+        });
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
