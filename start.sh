9e9f738ea9a87de1b5859ac8ccdeeaa7921417ac
charlie on ubuntu 22.04 <charlie@elvenware.com>
2023-02-26 10:13:35 -0800

    Update main and make sure that it has the new files. We're completely deleting the old master branch. We are completely deleting the old snippets code, but we have moved them into their own branch. We have also saved the old man in the old master branchs. run git br -a just see the saved branches.

---
+++ start.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+if [ $# -eq 0 ]; then
+    >&2 echo "No arguments provided"
+    exit 1
+fi
+
+NAME=$1
+if [ -d "$NAME" ]; then
+    echo "Directory $NAME exists." 
+else
+    echo "Directory $NAME does not exist."
+    mkdir $NAME
+    cd $NAME
+    mkdir src
+    mkdir dist
+    cp ../assets/index.html ./index.html
+    cp ../assets/index.js ./src/index.js
+    cp ../assets/style.css ./src/style.css
+    cp ../assets/index.scss ./src/index.scss
+    cp ../assets/core.scss ./src/core.scss
+    cp ../assets/webpack.config.js ./webpack.config.js
+    cp ../assets/.gitignore ./.gitignore
+    npm init -y
+    npm install webpack webpack-cli sass --save-dev
+    npm install normalize.css
+    sass src/index.scss dist/style.css
+fi
+
