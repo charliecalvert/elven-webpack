17f70edcea5c2a8f1a5395b9bd8109f31342f01a
charlie on ubuntu 22.04 <charlie@elvenware.com>
2023-02-27 15:39:14 -0800

    set default npm license

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+# Uncomment the next lines to set the default license
+# npm config set init-license "MIT"
+
 NAME=$1
 if [ -d "$NAME" ]; then
     echo "Directory $NAME exists." 
@@ -20,10 +23,10 @@ else
     cp ../assets/core.scss ./src/core.scss
     cp ../assets/webpack.config.js ./webpack.config.js
     cp ../assets/.gitignore ./.gitignore
-    sed 's/"scripts": {/"scripts": {\n    "build": "webpack"/' package.json
     npm init -y
-    npm install webpack webpack-cli sass --save-dev
+    npm install webpack webpack-dev-server html-webpack-plugin webpack-cli sass --save-dev
     npm install normalize.css
     sass src/index.scss dist/style.css
+    sed -i 's/"scripts": {/"scripts": {\n    "build": "webpack",/' package.json
 fi
 
