8a0c40568d78b155820da17b0ad46f5a16ec0098
charlie on ubuntu 22.04 <charlie@elvenware.com>
2023-02-27 17:32:32 -0800

    clean up use colors

--- test-it.sh
+++ test-it.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
+COLORS=~/Git/JsObjects/Utilities/Templates/Scripts/Colors.sh
+LOCAL_COLORS=./Colors.sh
+
+if [ -f "$LOCAL_COLORS" ]; then
+    echo "Testing: File $COLORS exists." 
+else
+    echo "Testing: File $COLORS does not exist."
+    cp "$COLORS" .
+fi
+
+source ./Colors.sh
+
 TEST03="test03"
 
 if [ -d "$TEST03" ]; then
-    echo "Testing: Directory $TEST03 exists." 
+    echo -e ${Green}"Testing${Color_Off}: Directory $TEST03 exists." 
     rm -r test03
 else
     echo "Testing: Directory $TEST03 does not exist."
